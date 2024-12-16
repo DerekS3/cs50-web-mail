56cd3c922be9ae7831ab97cd5cdcfe363540341d
DS0eng <derekstrickland3@gmail.com>
2024-12-16 21:09:48 +0000

    Implemented preview_email and open_email functions

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -14,6 +14,7 @@ function compose_email() {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#email-element').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields
@@ -22,45 +23,94 @@ function compose_email() {
   document.querySelector('#compose-body').value = '';
 
   // Event listener for sending mail
-  document.querySelector('#send-email').onclick = send_email;
+  document.querySelector('#compose-form').onsubmit = send_email;
 }
 
 function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#email-element').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  fetch_emails(mailbox)
+}
+
+function open_email(email) {
+
+  // Show the email and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#email-element').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  email.read = true;
+
+  // Use email subject as page title
+  document.querySelector('#email-element').innerHTML = `
+    <h3>${email.subject}</h3>
+    <p> from: ${email.sender} ${email.timestamp} </p>
+    <p> to: ${email.recipients.join(', ')} </p>
+    <p> ${email.body}</p>
+  `;
+
+
+
+  console.log('This email has been opened')
 }
 
 function fetch_emails(mailbox) {
 
-  fetch('/emails/inbox')
+  fetch(`/emails/${mailbox}`)
   .then(response => response.json())
   .then(emails => {
     // Print emails
     console.log(emails);
 
     // ... do something else with emails ...
-    emails.forEach(display_email)
+    emails.forEach(email => {
+      preview_email(email, mailbox)
+    });
   });
 }
 
-function display_email(email) {
+
+
+function preview_email(email, mailbox) {
+
+  // Determine whether to show Sender or Recipient
+  let displayContact;
+  if (mailbox === 'sent') {
+    displayContact = `<strong>To:</strong> ${email.recipients.join(', ')}`;
+  } else {
+    displayContact = `<strong>From:</strong> ${email.sender}`;
+  }
 
   // Create email preview
   const emailElement = document.createElement('div');
-  emailElement.className = 'email-item';
-  emailElement.innerHTML = email.subject;
+  emailElement.classList.add('email-item');
+  emailElement.classList.toggle('email-read', email.read === true);
+  emailElement.classList.toggle('email-unread', email.read !== true);
+  emailElement.innerHTML = `
+    <p><small> ${displayContact} - ${email.timestamp} </small></p>
+    <p><strong> ${email.subject} </strong></p>
+    <p><small> </small></p>
+  `;
+
+  // Open email on click
+  emailElement.addEventListener('click', () => {
+    open_email(email)
+  });
 
   // Add post to DOM
   document.querySelector('#emails-view').append(emailElement)
 
 }
 
-function send_email() {
+function send_email(event) {
+  event.preventDefault();
 
   const recipients = document.querySelector('#compose-recipients').value;
   const subject = document.querySelector('#compose-subject').value;
